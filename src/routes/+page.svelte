67ddce6000786c076c3187699dad4bbcd4ed3075
Williamnumismatics <williamnumismatics@gmail.com>
2026-03-25 16:31:22 +1100

    favicon

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,8 +2,7 @@
     import { enhance } from '$app/forms';
 </script>
 
-
-<h1>Dashboard Name</h1>
+<div class="top-bar"></div>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
 <form method="post" action="?/login" use:enhance>
     <button >Click me</button>
